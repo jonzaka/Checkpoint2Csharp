c3dee1d0d55fad34a94155a304a5899d40ca6c5f
jonzaka <41811232+jonzaka@users.noreply.github.com>
2025-09-30 23:20:13 -0300

    Create Category.cs

---
+++ EfCrudApp/Models/Category.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EfCrudApp.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    [Required, MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public List<Product> Products { get; set; } = new();
+}
